5d216f632964f8e04c82c01c05bd006fdd24e6ee
rikonnect <chachaerickjo@gmail.com>
2022-03-06 20:16:54 +0300

    ft: add business logic for getting the day of the week based on the date selected

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 let userInput = document.querySelector("#userdata")
+let gender = userInput.gender.value
+let names = []
 
 
 userInput.addEventListener("submit", (e)=>{
     e.preventDefault()
 
     let name = userInput.username.value
-    let gender = userInput.gender.value
-    let date = userInput.dateofbirth.value
+    
+    //uses date object to get the date for input by the user
     let daya = userInput.dateofbirth.value
+
+    //instantiates the date object
     let d = new Date(daya)
+
+    //uses the .getDay() method to get the day of the week for the date entered
     siku = d.getDay()
-    mwezi = d.getMonth()
-    mwaka = d.getFullYear()
+    // mwezi = d.getMonth()
+    // mwaka = d.getFullYear()
     
 })
+validate = () =>{
+    if(gender == 'male'){
+        names = ['kwasi','kwadwo','kwabena','kwaku','yaw','kofi','kwame']
+    }
+    else if(gender == 'female'){
+        names = ['akosua','adwoa','abena','akua','yaa','afua','ama']
+    }
+    
+}
