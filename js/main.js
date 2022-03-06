1ee43f70832269352bcf1d5e28f29136dd2c75e0
rikonnect <chachaerickjo@gmail.com>
2022-03-06 17:48:32 +0300

    ft: add logic for getting the date elements entered by the user

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-let userdata = document.
+let userInput = document.querySelector("#userdata")
+
+
+userInput.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    let name = userInput.username.value
+    let gender = userInput.gender.value
+    let date = userInput.dateofbirth.value
+    let daya = userInput.dateofbirth.value
+    let d = new Date(daya)
+    siku = d.getDay()
+    mwezi = d.getMonth()
+    mwaka = d.getFullYear()
+    
+})
