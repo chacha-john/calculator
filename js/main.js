4c41df83cb4a40868f73586d927fd3a3505212c8
rikonnect <chachaerickjo@gmail.com>
2022-03-07 17:40:50 +0300

    ft: add an array with fun facts to be diplayed randomly

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 let userInput = document.querySelector("#userdata")
+let ff = document.querySelector("#funfact")
 
 //names arrays
 male = ['Kwasi','Kwadwo','Kwabena','KwaKu','Yaw','Kofi','Kwame']
 female = ['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Ama']
+funFacts = ['Ghana is at the centre of the world! Both close to the equator and the Greenwich Meridian!','Ghana existed as an empire from the seventh to the thirteenth century!','Ghana was the first country to get independence from colonization in the year 1957 under Kwame Nkurumah as the president!','The Akan people of Ghana still practice inheritance based on matriarchial lineage!','For the Akan tribe, the first names of children are based on the day of the week they were born and their gender!','Ghanaians make very fancy coffins - mostly artistic - because they believe there is life after death and the coffins serves to make a good impression for the deead when they arrive in their next life!','The popular musical style of the mid 20th century originated in Ghana. Highlife today has evolved to electronic Twi music!','Soccer is the national pastime in Ghana which has made the sport so successful since the 1940s!']
 
 
-userInput.addEventListener("submit", (e)=>{
+window.addEventListener("submit", (e)=>{
     e.preventDefault()
     
     
     validate()
     
+})
+window.addEventListener("load",(e)=>{
+    document.querySelector("#fact").textContent = funFacts[Math.floor(Math.random()*funFacts.length)]
 })
 validate = () =>{
     //instantiates the gender
